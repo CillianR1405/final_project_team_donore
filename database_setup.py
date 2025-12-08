d46303757bb293f4a42d50aba15043b645d61672
youngej3 <jack.younger3@mail.dcu.ie>
2025-12-08 22:40:34 +0000

    spotify

--- database_setup.py
+++ database_setup.py
@@ -5,6 +5,13 @@ base_directory = os.path.dirname(os.path.abspath(__file__))
 database_folder = os.path.join(base_directory, "music_database")
 os.makedirs(database_folder, exist_ok=True)
 database_path = os.path.join(database_folder, "music.db")
+
 conn = sqlite3.connect(database_path)
 cur = conn.cursor()
 
+cur.execute("""
+CREATE TABLE IF NOT EXISTS spotify_artists (
+    id TEXT PRIMARY KEY,
+    name TEXT UNIQUE
+)
+""")
